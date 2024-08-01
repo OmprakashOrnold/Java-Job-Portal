8b640e0def477b18181047186f0df6198a3cbb17
Bablukanna6 <Bablukanna6@gmail.com>
2024-08-01 10:30:12 +0530

    Initial commit

---
+++ src/main/java/com/job/portal/JavaJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.job.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJobPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( JavaJobPortalApplication.class, args );
+    }
+
+}
